628ebd4fdf88eaf3535156e6c5548922045eefec
toqokoDev <toqokoDev@gmail.com>
2025-08-22 19:14:48 +0300

    feat: merdge

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Tennis-bot
 Телеграмм бот для игры в теннис и не только
+
+команда для запуска:
+docker build -t tennis-bot . && docker run -d -p 8000:8000 --name tennis-container-bot tennis-bot
--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
-TOKEN = "8070477161:AAFPtQdUtqPGWxlrUyG26opffTuPCi7pq4w"
+TOKEN = "8265226124:AAFs5XaIerZIKOtnfCEkPICvVAdaQRAW08U"
 ADMIN_ID = 0 # ID администратора
 SUBSCRIPTION_PRICE = 300  # Цена подписки в рублях
-BOT_USERNAME = "lab_bglk_bot"
+BOT_USERNAME = "tennis_playbot"
 
 SHOP_ID = "1150013"
 SECRET_KEY = "test_n0Z7mNbY1qZJk7KakSPjDfULRhdGc1-GIziY3noew4Q"
